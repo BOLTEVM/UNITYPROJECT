38e374bc937243b1ac705def1da54a6318109332
j_b0gie <wxzvrd@protonmail.com>
2023-05-07 22:57:23 +0100

    forgot a stragler

--- Assets/_Scripts/DoorSwitch.cs
+++ Assets/_Scripts/DoorSwitch.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SkinnedMeshRenderer doorRenderer;
+    public void OpenDoor()
     {
-        
+        // Code to open the door
+        // Get the bone transforms for the door
+        Transform[] bones = doorRenderer.bones;
+
+        // Find the bone that controls the door's rotation
+        Transform doorBone = Array.Find(bones, bone => bone.name == "Close");
+
+        // Rotate the bone to open the door
+        doorBone.localRotation = Quaternion.Euler(0f, 90f, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            // Call the OpenDoor method to open the door
+            OpenDoor();
+        }
     }
+
 }
